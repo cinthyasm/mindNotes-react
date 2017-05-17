dc154d7a07d336d9fe6f81fc6231c4ff63e5bfb8
Dilan Quesada Rojas <di.quesada93@gmail.com>
2017-05-17 14:29:21 -0600

    Font awesome addded

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,10 @@ module.exports = {
           'css-loader',
           'sass-loader',
         ],
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
       }
     ]
   },
